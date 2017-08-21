5bffcab404e925848f332272b52bccc1f37b776e
天青色 <ice45571@163.com>
2017-08-21 20:40:18 +0800

    2017年08月21日20:40:06 修复recyclerView不显示问题

--- app/src/main/java/com/yusion/shanghai/yusion/ui/update/UpdateImgsFragment.java
+++ app/src/main/java/com/yusion/shanghai/yusion/ui/update/UpdateImgsFragment.java
@@ -1,20 +1,29 @@
 package com.yusion.shanghai.yusion.ui.update;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.yusion.shanghai.yusion.R;
+import com.yusion.shanghai.yusion.adapter.UploadLabelListAdapter;
 import com.yusion.shanghai.yusion.base.BaseFragment;
+import com.yusion.shanghai.yusion.bean.upload.UploadLabelItemBean;
+import com.yusion.shanghai.yusion.ui.info.UploadLabelListActivity;
+import com.yusion.shanghai.yusion.ui.info.UploadListActivity;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class UpdateImgsFragment extends BaseFragment {
+
+    private UploadLabelListAdapter mAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
