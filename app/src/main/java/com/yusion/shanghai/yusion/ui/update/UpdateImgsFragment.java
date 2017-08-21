8b86db27eec8e3ec1b906807307c94a38752efff
天青色 <ice45571@163.com>
2017-08-21 16:10:30 +0800

    2017年08月21日16:10:25 fab

---
+++ app/src/main/java/com/yusion/shanghai/yusion/ui/update/UpdateImgsFragment.java
@@ -0,0 +1,29 @@
+package com.yusion.shanghai.yusion.ui.update;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.yusion.shanghai.yusion.R;
+import com.yusion.shanghai.yusion.base.BaseFragment;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class UpdateImgsFragment extends BaseFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_update_img, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
