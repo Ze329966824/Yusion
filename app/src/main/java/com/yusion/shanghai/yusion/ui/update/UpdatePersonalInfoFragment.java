ab801af6794c2fae4b5b3d1c7a32169d2df1ab38
ze329966824 <ze329966824>
2017-08-22 18:59:40 +0800

    个人资料部分WheelViewUtil 2017年8月22日18:59:07

--- app/src/main/java/com/yusion/shanghai/yusion/ui/update/UpdatePersonalInfoFragment.java
+++ app/src/main/java/com/yusion/shanghai/yusion/ui/update/UpdatePersonalInfoFragment.java
@@ -27,14 +27,67 @@ public class UpdatePersonalInfoFragment extends BaseFragment {
     private List<String> incomeextarlist = new ArrayList<String>() {{
         add("工资");
     }};
+    private List<String> sexlist = new ArrayList<String>() {{
+        add("男");
+        add("女?");
+    }};
+    private List<String> educationlist = new ArrayList<String>() {{
+        add("大专");
+        add("本科");
+        add("硕士");
+        add("博士");
+    }};
+    private List<String> housetypelist = new ArrayList<String>() {{
+        add("商品房");
+        add("自建房");
+    }};
+    private List<String> houseownerlist = new ArrayList<String>() {{
+        add("父母");
+        add("恋人");
+    }};
 
     public static int UPDATE_INCOME_FROME_INDEX;
     public static int UPDATE_EXTRA_INCOME_FROME_INDEX;
+    public static int UPDATE_SEX_INDEX;
+    public static int UPDATE_EDUCATION_INDEX;
+    public static int UPDATE_HOUSE_TYPE_INDEX;
+    public static int UPDATE_HOUSE_OWNER_RELATION_INDEX;
+    public static int UPDATE_URG_RELATION_INDEX1;
+    public static int UPDATE_URG_RELATION_INDEX2;
+
 
     private LinearLayout income_from_lin;
     private LinearLayout income_extra_from_lin;
+
     private TextView income_from_tv;
     private TextView income_extra_from_tv;
+
+    private LinearLayout personal_info_gender_lin;
+    private TextView personal_info_gender_tv;
+
+    private LinearLayout personal_info_reg_lin;
+    private TextView personal_info_reg_tv;
+
+    private LinearLayout personal_info_education_lin;
+    private TextView personal_info_education_tv;
+
+    private LinearLayout personal_info_current_address_lin;
+    private TextView personal_info_current_address_tv;
+
+    private LinearLayout personal_info_current_address1_lin;
+    private TextView personal_info_current_address1_tv;
+
+    private LinearLayout personal_info_house_type_lin;
+    private TextView personal_info_house_type_tv;
+
+    private LinearLayout personal_info_house_owner_relation_lin;
+    private TextView personal_info_house_owner_relation_tv;
+
+    private LinearLayout personal_info_urg_relation1_lin;
+    private TextView personal_info_urg_relation1_tv;
+
+    private LinearLayout personal_info_urg_relation2_lin;
+    private TextView personal_info_urg_relation2_tv;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -42,7 +95,7 @@ public class UpdatePersonalInfoFragment extends BaseFragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_update_personal_info, container, false);
 
-
+        //选择收入来源
         income_from_lin = (LinearLayout) view.findViewById(R.id.personal_info_income_from_lin);
         income_from_tv = (TextView) view.findViewById(R.id.personal_info_income_from_tv);
         income_from_lin.setOnClickListener(new View.OnClickListener() {
@@ -79,7 +132,7 @@ public class UpdatePersonalInfoFragment extends BaseFragment {
             }
         });
 
-
+        //选择额外收入来源
         income_extra_from_lin = (LinearLayout) view.findViewById(R.id.personal_info_extra_income_from_lin);
         income_extra_from_tv = (TextView) view.findViewById(R.id.personal_info_extra_income_from_tv);
         income_extra_from_lin.setOnClickListener(new View.OnClickListener() {
@@ -105,7 +158,177 @@ public class UpdatePersonalInfoFragment extends BaseFragment {
             }
         });
 
+        //选择性别
+        personal_info_gender_lin = (LinearLayout) view.findViewById(R.id.personal_info_gender_lin);
+        personal_info_gender_tv = (TextView) view.findViewById(R.id.personal_info_gender_tv);
+        personal_info_gender_lin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                WheelViewUtil.showWheelView(sexlist,
+                        UPDATE_SEX_INDEX,
+                        personal_info_gender_lin,
+                        personal_info_gender_tv,
+                        "请选择",
+                        new WheelViewUtil.OnSubmitCallBack() {
+                            @Override
+                            public void onSubmitCallBack(View clickedView, int selectedIndex) {
+                                UPDATE_SEX_INDEX = selectedIndex;
+                            }
+                        }
+                );
+            }
+        });
 
+        //选择户籍地
+        personal_info_reg_lin = (LinearLayout) view.findViewById(R.id.personal_info_reg_lin);
+        personal_info_reg_tv = (TextView) view.findViewById(R.id.personal_info_reg_tv);
+        personal_info_reg_lin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                WheelViewUtil.showCityWheelView(getClass().getSimpleName(),
+                        personal_info_reg_lin,
+                        personal_info_reg_tv,
+                        "请选择所在地区",
+                        new WheelViewUtil.OnCitySubmitCallBack() {
+                            @Override
+                            public void onCitySubmitCallBack(View clickedView, String city) {
+
+                            }
+                        }
+                );
+            }
+        });
+
+
+        //选择学历
+        personal_info_education_lin = (LinearLayout) view.findViewById(R.id.personal_info_education_lin);
+        personal_info_education_tv = (TextView) view.findViewById(R.id.personal_info_education_tv);
+        personal_info_education_lin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                WheelViewUtil.showWheelView(educationlist,
+                        UPDATE_EDUCATION_INDEX,
+                        personal_info_education_lin,
+                        personal_info_education_tv,
+                        "请选择",
+                        new WheelViewUtil.OnSubmitCallBack() {
+                            @Override
+                            public void onSubmitCallBack(View clickedView, int selectedIndex) {
+                                UPDATE_EDUCATION_INDEX = selectedIndex;
+                            }
+                        });
+            }
+        });
+
+        //现在地址
+        personal_info_current_address_lin = (LinearLayout) view.findViewById(R.id.personal_info_current_address_lin);
+        personal_info_current_address_tv = (TextView) view.findViewById(R.id.personal_info_current_address_tv);
+        personal_info_current_address_lin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                WheelViewUtil.showCityWheelView(getClass().getSimpleName(),
+                        personal_info_current_address_lin,
+                        personal_info_current_address_tv,
+                        "请选择所在地区",
+                        new WheelViewUtil.OnCitySubmitCallBack() {
+                            @Override
+                            public void onCitySubmitCallBack(View clickedView, String city) {
+                            }
+                        }
+                );
+            }
+        });
+
+        //详细地址  ？？？？？？？？？？？
+        personal_info_current_address1_lin = (LinearLayout) view.findViewById(R.id.personal_info_current_address1_lin);
+
+        personal_info_current_address1_tv = (TextView) view.findViewById(R.id.personal_info_current_address1_tv);
+        personal_info_current_address1_lin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (personal_info_current_address_tv != null) {
+
+                }
+            }
+        });
+
+        //房屋类型
+        personal_info_house_type_lin = (LinearLayout) view.findViewById(R.id.personal_info_house_type_lin);
+        personal_info_house_type_tv = (TextView) view.findViewById(R.id.personal_info_house_type_tv);
+        personal_info_house_type_lin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                WheelViewUtil.showWheelView(housetypelist,
+                        UPDATE_HOUSE_TYPE_INDEX,
+                        personal_info_house_type_lin,
+                        personal_info_house_type_tv,
+                        "请选择",
+                        new WheelViewUtil.OnSubmitCallBack() {
+                            @Override
+                            public void onSubmitCallBack(View clickedView, int selectedIndex) {
+                                UPDATE_HOUSE_TYPE_INDEX = selectedIndex;
+                            }
+                        });
+            }
+        });
+
+        //与申请人关系
+        personal_info_house_owner_relation_lin = (LinearLayout) view.findViewById(R.id.personal_info_house_owner_relation_lin);
+        personal_info_house_owner_relation_tv = (TextView) view.findViewById(R.id.personal_info_house_owner_relation_tv);
+        personal_info_house_owner_relation_lin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                WheelViewUtil.showWheelView(houseownerlist,
+                        UPDATE_HOUSE_OWNER_RELATION_INDEX,
+                        personal_info_house_owner_relation_lin,
+                        personal_info_house_owner_relation_tv,
+                        "请选择",
+                        new WheelViewUtil.OnSubmitCallBack() {
+                            @Override
+                            public void onSubmitCallBack(View clickedView, int selectedIndex) {
+                                UPDATE_HOUSE_OWNER_RELATION_INDEX = selectedIndex;
+                            }
+                        });
+            }
+        });
+
+        personal_info_urg_relation2_lin = (LinearLayout) view.findViewById(R.id.personal_info_urg_relation2_lin);
+        personal_info_urg_relation2_tv = (TextView) view.findViewById(R.id.personal_info_urg_relation2_tv);
+        personal_info_urg_relation2_lin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                WheelViewUtil.showWheelView(houseownerlist,
+                        UPDATE_URG_RELATION_INDEX2,
+                        personal_info_urg_relation2_lin,
+                        personal_info_urg_relation2_tv,
+                        "请选择",
+                        new WheelViewUtil.OnSubmitCallBack() {
+                            @Override
+                            public void onSubmitCallBack(View clickedView, int selectedIndex) {
+                                UPDATE_URG_RELATION_INDEX2 = selectedIndex;
+                            }
+                        });
+            }
+        });
+
+        personal_info_urg_relation1_lin = (LinearLayout) view.findViewById(R.id.personal_info_urg_relation1_lin);
+        personal_info_urg_relation1_tv = (TextView) view.findViewById(R.id.personal_info_urg_relation1_tv);
+        personal_info_urg_relation1_lin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                WheelViewUtil.showWheelView(houseownerlist,
+                        UPDATE_URG_RELATION_INDEX1,
+                        personal_info_urg_relation1_lin,
+                        personal_info_urg_relation1_tv,
+                        "请选择",
+                        new WheelViewUtil.OnSubmitCallBack() {
+                            @Override
+                            public void onSubmitCallBack(View clickedView, int selectedIndex) {
+                                UPDATE_URG_RELATION_INDEX1 = selectedIndex;
+                            }
+                        });
+            }
+        });
         return view;
 
     }
