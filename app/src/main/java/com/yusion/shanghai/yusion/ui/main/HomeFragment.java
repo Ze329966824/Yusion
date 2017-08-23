de34e0c98fa5254dc4b430eb380e4c93301d5ba4
aa <1043261302@qq.com>
2017-08-23 15:07:21 +0800

    2017年08月23日15:07:13

--- app/src/main/java/com/yusion/shanghai/yusion/ui/main/HomeFragment.java
+++ app/src/main/java/com/yusion/shanghai/yusion/ui/main/HomeFragment.java
@@ -14,6 +14,7 @@ import com.yusion.shanghai.yusion.R;
 import com.yusion.shanghai.yusion.base.BaseFragment;
 import com.yusion.shanghai.yusion.bean.auth.CheckUserInfoResp;
 import com.yusion.shanghai.yusion.ui.apply.ApplyActivity;
+import com.yusion.shanghai.yusion.ui.order.FinancePlanActivity;
 import com.yusion.shanghai.yusion.ui.update.InfoListActivity;
 import com.yusion.shanghai.yusion.ui.update.UpdateSecuritySpouseInfoActivity;
 import com.yusion.shanghai.yusion.ui.update.UpdateSpouseInfoActivity;
@@ -43,6 +44,14 @@ public class HomeFragment extends BaseFragment {
         bottomBtn = (Button) view.findViewById(R.id.home_bottom_btn);
         Button btn = (Button) view.findViewById(R.id.btn_test);
         Button btn2 = (Button) view.findViewById(R.id.btn_test2);
+        Button btn4 = (Button) view.findViewById(R.id.btn4);
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mContext, FinancePlanActivity.class);
+                startActivity(intent);
+            }
+        });
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
