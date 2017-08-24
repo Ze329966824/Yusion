1cf11ef8625075f7459aa68b08b3017f2290018e
ze329966824 <ze329966824>
2017-08-24 09:51:48 +0800

    2017年8月24日09:51:38

--- app/src/main/java/com/yusion/shanghai/yusion/retrofit/service/UserService.java
+++ app/src/main/java/com/yusion/shanghai/yusion/retrofit/service/UserService.java
@@ -14,6 +14,7 @@ import retrofit2.http.Query;
 
 public interface UserService {
     //http://192.168.0.214:8000/api/client/client_guarantor_info_list/
+    //用户资料列表类型接口
     @GET("api/client/client_guarantor_info_list")
     Call<BaseResult<ListCurrentTpye>> getListCurrentTpye();
 }
