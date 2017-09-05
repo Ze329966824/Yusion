29bd9d6df5e620c65aa733835a265864a68e260c
天青色 <ice45571@163.com>
2017-09-05 12:18:12 +0800

    2017年09月05日12:18:10

---
+++ app/src/main/java/com/yusion/shanghai/yusion/retrofit/callback/OnMultiDataCallBack.java
@@ -0,0 +1,9 @@
+package com.yusion.shanghai.yusion.retrofit.callback;
+
+/**
+ * Created by ice on 2017/9/5.
+ */
+
+public interface OnMultiDataCallBack<A, B> {
+    void onMultiDataCallBack(A a, B b);
+}
