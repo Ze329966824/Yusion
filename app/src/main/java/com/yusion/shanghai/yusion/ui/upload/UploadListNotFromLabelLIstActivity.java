8de9dcfd00430655df6c935eee667963b55a1ccb
aa <1043261302@qq.com>
2017-09-15 16:53:20 +0800

    2017年09月15日16:52:52 添加raw_url 字段

--- app/src/main/java/com/yusion/shanghai/yusion/ui/upload/UploadListNotFromLabelLIstActivity.java
+++ app/src/main/java/com/yusion/shanghai/yusion/ui/upload/UploadListNotFromLabelLIstActivity.java
@@ -162,7 +162,7 @@ public class UploadListNotFromLabelLIstActivity extends BaseActivity {
                     if (!TextUtils.isEmpty(item.local_path)) {
                         imgUrl = item.local_path;
                     } else {
-                        imgUrl = item.s_url;
+                        imgUrl = item.raw_url;
                     }
                     previewImg(findViewById(R.id.preview_anchor), imgUrl);
 
