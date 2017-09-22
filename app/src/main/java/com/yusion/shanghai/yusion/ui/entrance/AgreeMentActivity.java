9a1155a1218d7b434846b87d50181a546d2b9f6a
aa <1043261302@qq.com>
2017-09-22 17:16:40 +0800

    2017年09月22日17:15:04 协议改为web形式

--- app/src/main/java/com/yusion/shanghai/yusion/ui/entrance/AgreeMentActivity.java
+++ app/src/main/java/com/yusion/shanghai/yusion/ui/entrance/AgreeMentActivity.java
@@ -5,13 +5,20 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 
 import com.yusion.shanghai.yusion.R;
+import com.yusion.shanghai.yusion.YusionApp;
 import com.yusion.shanghai.yusion.base.BaseActivity;
 import com.yusion.shanghai.yusion.bean.auth.CheckIsAgreeReq;
 import com.yusion.shanghai.yusion.retrofit.api.AuthApi;
 import com.yusion.shanghai.yusion.retrofit.callback.OnCodeAndMsgCallBack;
+import com.yusion.shanghai.yusion.utils.LoadingUtils;
 import com.yusion.shanghai.yusion.utils.SharedPrefsUtil;
 import com.yusion.shanghai.yusion.widget.TitleBar;
 
@@ -19,19 +26,29 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class AgreeMentActivity extends BaseActivity {
+public class AgreeMentActivity extends BaseActivity implements View.OnClickListener {
     private TitleBar titleBar;
     private Button acceptBtn;
     private Button noAcceptBtn;
     private CheckIsAgreeReq req;
+    private WebView webView;
+    private String url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agree_ment);
         titleBar = initTitleBar(this, "《客户信息获取、保密政策》").setLeftVisible(false);
+        webView = (WebView) findViewById(R.id.webView);
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setCacheMode(WebSettings.LOAD_NO_CACHE);
         acceptBtn = (Button) findViewById(R.id.accept);
         noAcceptBtn = (Button) findViewById(R.id.no_accept);
+        url = YusionApp.CONFIG_RESP.confident_policy_url;
+        webView.loadUrl(url);
+
+        initWebView();
 
         acceptBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -73,5 +90,23 @@ public class AgreeMentActivity extends BaseActivity {
     public void finish() {
         super.finish();
         overridePendingTransition(0, R.anim.activity_close);
+    }
+
+    private void initWebView() {
+        webView.setOnClickListener(this);
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                //返回值是true的时候控制去WebView打开，为false调用系统浏览器或第三方浏览器
+                view.loadUrl(url);
+                return true;
+            }
+        });
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                LoadingUtils.createLoadingDialog(AgreeMentActivity.this);
+            }
+        });
     }
 }
