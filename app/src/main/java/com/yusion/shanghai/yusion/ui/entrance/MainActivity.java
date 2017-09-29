4a6736883b3b97a5dc12f1bab571328f192971c4
aa <1043261302@qq.com>
2017-09-29 10:44:12 +0800

    2017年09月29日10:44:07

--- app/src/main/java/com/yusion/shanghai/yusion/ui/entrance/MainActivity.java
+++ app/src/main/java/com/yusion/shanghai/yusion/ui/entrance/MainActivity.java
@@ -1,7 +1,6 @@
 package com.yusion.shanghai.yusion.ui.entrance;
 
 import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
